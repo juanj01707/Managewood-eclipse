e4f626a4e757af489982f0db2152dedd8159c0d8
juanj01707 <90587297+juanj01707@users.noreply.github.com>
2023-06-10 20:24:09 -0500

    Molde

---
+++ workspace/ManageWoodApi/src/test/java/co/com/uco/managewood/api/init/ManageWoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.uco.managewood.api.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageWoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
